e16fd132d181d11bc8e8d5a3a06600abc141a987
oshanpandit <oshanpandit@gmail.com>
2025-07-15 17:36:28 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,23 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: "standalone",
+// }
+
+// export default nextConfig;
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
-}
+    devIndicators: false,
+    experimental: {
+        authInterrupts: true,
+    },
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            config.plugins = [...config.plugins, new PrismaPlugin()];
+        }
+        return config;
+    },
+};
 
 export default nextConfig;
